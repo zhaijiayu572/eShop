9fb9a0c70a2726d9ce3056251c741c3d440bd45b
zhaijiayu572 <zjypersonal@163.com>
2017-04-13 18:21:40 +0800

    初步建立

---
+++ application/views/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>e_shop</title>
+    <style>
+        #eshop-header{
+            width: 100%;
+            height: 400px;
+            background: #00CC00;
+        }
+    </style>
+</head>
+<body>
+    <header id="eshop-header">
+        <div class="wrapper">
+            <div class="top-show">
+                <div class="login-access">LOGIN</div>
+                <div class="eshop-logo">NEW FASHION</div>
+                <div class="my-cart">
+                    <div class="cart-info"></div>
+                    <div class="clear-cary"><a href="#">empty cart</a></div>
+                </div>
+            </div>
+            <ul class="nav">
+                <li>HOME</li>
+                <li>WOMEN</li>
+                <li>MEN</li>
+                <li>ABOUT US</li>
+                <li>BLOG</li>
+                <li>SHOP ONLINE</li>
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
